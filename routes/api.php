3e8f9bc3bb97929df81dfa7efde26298868e6743
fzhnf <faizahnaf52@gmail.com>
2023-07-04 18:11:46 +0800

    alhamdulillah ngga nge-stuck lagi

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,5 +17,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-
 });
+
+Route::get('tasks', [TaskController::class,'index']);
+
+// Route::get('/tasks/{param}', function ($param) use ($taskList) {
+//     try {
+//         return $taskList[$param];
+//     } catch (\Throwable $th) {
+//         return response()->json([
+//             'message' => 'Task not found',
+//         ], 404);
+//     }
+// });
+Route::get('tasks/{id}', [TaskController::class, 'show']);
+
+Route::post('tasks', [TaskController::class, 'store']);
+// { return request()->all();});
+
+
+Route::patch('tasks/{id}', [TaskController::class, 'update']);
+
+Route::delete('tasks/{id}', [TaskController::class, 'destroy']);
